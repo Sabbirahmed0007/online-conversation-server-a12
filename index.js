855836f3cad635a8b794c91f5cb110d8593f53c2
Sabbirahmed0007 <>
2023-12-04 23:03:23 +0600

    added dashboard and admin and user related apis

--- index.js
+++ index.js
@@ -41,15 +41,75 @@ async function run() {
     const postCollection = client.db('data').collection('posts');
     const usersCollection = client.db('data').collection('users');
     const tagsCollection = client.db('data').collection('tags');
+    const memberShipCollection = client.db('data').collection('membership');
     const announcementsCollection = client.db('data').collection('announcements');
+
+
+     // jwt token related api
+     app.post('/jwt', async(req, res)=>{
+      const user = req.body;
+    
+      const token= jwt.sign(user, process.env.ACCESS_SECRET_TOKEN, {expiresIn: '2hr'}) ;  
+      res.send({token});
+    })
+
+
+    //// middleware 
+
+    const verifyToken =(req, res, next)=>{
+      console.log(req.headers.authorization);
+      if(!req.headers.authorization){
+        return res.status(401).send({message: 'Unauthorized access'});
+      }
+      const token = req.headers.authorization.split(' ')[1];
+      console.log(token);
+      jwt.verify(token, process.env.ACCESS_SECRET_TOKEN, (error, decoded)=>{
+
+        if(error){
+          return res.status(401).send('Unauthorized access')
+        }
+        req.decoded = decoded;
+        next();
+      })
+      
+    }
+
+    // use verify admin after veryfy token 
+    const verifyAdmin =async(req, res, next)=>{
+      const email = req.decoded.email;
+      const query = {email: email};
+      const user = await usersCollection.findOne(query);
+      const isAdmin= user?.role === 'admin';
+      if(!isAdmin){
+        return res.status(403).send({message: 'forbidden access'});
+      }
+      next()
+    }
+
+
 
 
 
     // User related api-------------------------------------
 
-    app.get('/users', async(req, res)=>{
+    app.get('/users',verifyToken, verifyAdmin, async(req, res)=>{
       const result = await usersCollection.find().toArray();
       res.send(result);
+    })
+
+
+    app.get('/users/admin/:email', verifyToken, async(req, res)=>{
+      const email = req.params.email;
+      if(email !== req.decoded.email){
+        return res.status(403).send({message : 'forbidden access'})
+      }
+      const query = {email: email};
+      const user= await usersCollection.findOne(query);
+      let admin =false;
+      if(user){
+        admin = user?.role === 'admin';
+      }
+      res.send({admin});
     })
     
 
@@ -74,7 +134,7 @@ async function run() {
           role: 'admin',
         }
       }
-      const result = await usersCollection.updateOne (filter,updatedDoc);
+      const result = await usersCollection.updateOne (filter, updatedDoc);
       res.send(result);
     })
 
@@ -92,9 +152,15 @@ async function run() {
       res.send(result);
     })
 
+    app.post('/tags', async(req, res)=>{
+      const data = req.body;
+      const result = await tagsCollection.insertOne(data);
+      res.send(result);
+    })
+
     /// announcements related api -------------
 
-    app.get('/announcements', async(req, res)=>{
+    app.get('/announcements',  async(req, res)=>{
       const result = await announcementsCollection.find().toArray();
       res.send(result);
     })
@@ -104,11 +170,79 @@ async function run() {
       res.send([count]);
     })
 
-    app.post('/createAnnouncement', async(req, res)=>{
+    app.post('/createAnnouncement',verifyToken, verifyAdmin, async(req, res)=>{
       const query = req.body;
       const result = await announcementsCollection.insertOne(query);
       res.send(result);
     })
+
+
+    // membership related api
+    app.get('/primiumMemberShip', async(req, res )=>{
+      const email = req.query.email;
+      const query = {email: email};
+      const result = await memberShipCollection.find(query).toArray();
+      res.send(result); 
+    })
+
+    ///// Post related api
+    app.get('/posts', async(req, res)=>{
+      const page = parseInt(req.query.page);
+      const size = parseInt(req.query.size);
+      const result= await postCollection.find().toArray();
+      res.send(result);
+      
+    })
+
+    // app.get('/myPosts/:email', async(req, res)=>{
+    app.get('/myPosts/:email', async(req, res)=>{
+      const email = req.query.email;
+      // const email = req.params.email;
+      const filter = {email: email};
+      const result = await postCollection.find(filter).toArray();
+      res.send(result);
+    })
+
+    app.get('/postDetails/:id', async(req, res)=>{
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)};
+      const result = await postCollection.findOne(query);
+      console.log(result)
+      res.send(result);
+    })
+
+/// My post s delete
+    app.delete('/myPosts/:id', async(req, res)=>{
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)};
+      const result = await postCollection.deleteOne(query);
+      res.send(result);
+    })
+
+
+     /// Pagination data
+     app.get('/allPosts', async(req, res)=>{
+      const count =await postCollection.estimatedDocumentCount();
+      res.send({count});
+  })
+
+    app.post('/addposts', async(req, res)=>{
+      const query = req.body;
+      query.createdAt = new Date();
+      const result = await postCollection.insertOne(query);
+      res.send(result);
+    })
+
+
+    ///// Admin home stats
+    app.get('/adminStats', async(req, res)=>{
+      const users = await usersCollection.estimatedDocumentCount();
+      const posts = await postCollection.estimatedDocumentCount();
+
+      res.send({users, posts});
+    })
+
+    
 
 
 
